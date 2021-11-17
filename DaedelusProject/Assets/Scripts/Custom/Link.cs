55e7a47af07a0fb232849de8b5ea401cdf7f46a5
Otus, Cactus <mtmpafl@gmail.com>
2021-11-17 12:07:10 +0100

    [FEAT] Link has reference to room it's in and roomo it's leading to

--- DaedelusProject/Assets/Scripts/Custom/Link.cs
+++ DaedelusProject/Assets/Scripts/Custom/Link.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Link : MonoBehaviour
+public class Link
 {
     public bool hasLock = false;
     public Node[] nodes = new Node[2];
+    public LinkPos position;
 
-    Link()
+    public Link(LinkPos newPos, Node parentNode)
     {
-        
+        position = newPos;
+        nodes[0] = parentNode;
     }
+}
+public enum LinkPos
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
 }
